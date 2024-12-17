71e89d67a8e7f8531e6cdf5aba9a01f939365309
Jessica Mitchell <j.mitchell@fz-juelich.de>
2024-12-17 12:26:35 +0100

    update link

--- doc/sphinx-apidoc/conf.py
+++ doc/sphinx-apidoc/conf.py
@@ -155,7 +155,8 @@ mathjax_path = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-html_baseurl = 'https://nestml.readthedocs.io/en/latest/'
+html_baseurl = 'https://nestml-test.readthedocs.io/'
+sitemap_url_scheme = "https://nestml-test.readthedocs.io/en/latest"
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
